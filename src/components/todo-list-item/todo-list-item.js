5036980d5709683f9dce11e739c1ec7e1107f7a3
unknown <ilya.trofimov.01@gmail.com>
2021-03-15 00:50:33 +0200

    refactored

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -3,7 +3,8 @@ import './todo-list-item.css';
 
 const TodoListItem = ({label, important = false}) => {
     const style =  {
-        color: important ? 'tomato' : 'black' 
+        color: important ? 'steelblue' : 'black',
+        fontWeight: important ? 'bold' : 'normal'
     }
     return (
     <span style = {style} 
